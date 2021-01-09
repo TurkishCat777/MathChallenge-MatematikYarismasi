5b778d35c245ef0ac17063f5c51daa11b51a04b5
Niko Niyazi T*** <71526347+TurkishCat777@users.noreply.github.com>
2021-01-09 13:03:22 +0300

    Add files via upload

---
+++ Matematik Yarışması.py
@@ -0,0 +1,53 @@
+s1=input("Başlamak için enter'a basın.")
+
+print("=================================\n=================================\n=================================\n=================================\n")
+x1=int(input("13+9 "))
+if x1==22:
+    print("========2. Aşama========")
+else:
+    YanlışGirdin
+x2=int(input("2+177 "))
+if x2==179:
+    print("========3. Aşama========")
+else:
+    YanlışGirdin
+x3=int(input("4*9 "))
+if x3==36:
+    print("========4. Aşama========")
+else:
+    YanlışGirdin
+x4=int(input("11*4 "))
+if x4==44:
+    print("========5. Aşama========")
+else:
+    YanlışGirdin
+x5=input("13 bölü 3 işlemi kalansız bölünebilir mi? Bölünebilir ise e yazın. Değil ise h yazın.")
+if x5=="H" or x5=="h":
+    print("========6. Aşama========")
+else:
+    YanlışGirdin
+x6=int(input("150 bölü 3 "))
+if x6==50:
+    print("========7. Aşama========")
+else:
+    YanlışGirdin
+x7=int(input("(23+30)*2 "))
+if x7==106:
+    print("========8. Aşama========")
+else:
+    YanlışGirdin
+x8=int(input("247+?=398 "))
+if x8==151:
+    print("========9. Aşama========")
+else:
+    YanlışGirdin
+x9=int(input("7*7"))
+if x9==49:
+    print("========Son Aşama========")
+else:
+    YanlışGirdin
+x10_SON=int(input("37 bölü 6: Kalanı yazın. "))
+if x10_SON==1:
+    print("|=================================\=================================\=================================\=================================|")
+    print("                                                                Kazandın!")
+    print("|=================================\=================================\=================================\=================================|")
---
+++ Math Challenge.py
@@ -0,0 +1,54 @@
+s1=input("To start, press enter.")
+
+print("=================================\n=================================\n=================================\n=================================\n")
+x1=int(input("13+9 "))
+if x1==22:
+    print("========Level 2========")
+else:
+    Wrong
+x2=int(input("2+177 "))
+if x2==179:
+    print("========Level 3========")
+else:
+    Wrong
+x3=int(input("4*9 "))
+if x3==36:
+    print("========Level 4========")
+else:
+    Wrong
+x4=int(input("11*4 "))
+if x4==44:
+    print("========Level 5========")
+else:
+    Wrong
+x5=input("Can 13 be divived by 6 without remainder? If yes, press y and enter. If no, press n and enter. ")
+if x5=="n" or x5=="N":
+    print("========Level 6========")
+else:
+    Wrong 
+x6=int(input("150 divived by 3 "))
+if x6==50:
+    print("========Level 7========")
+else:
+    Wrong
+x7=int(input("(23+30)*2 "))
+if x7==106:
+    print("========Level 8========")
+else:
+    Wrong
+x8=int(input("247+?=398 "))
+if x8==151:
+    print("========Level 9========")
+else:
+    Wrong
+x9=int(input("7*7"))
+if x9==49:
+    print("========Level 10========")
+else:
+    YanlışGirdin
+x10_SON=int(input("37 divived by 6: What is the remainder?"))
+if x10_SON==1:
+    print("|=================================\=================================\=================================\=================================|")
+    print("                                                                 You Won!")
+    print("|=================================\=================================\=================================\=================================|")
+
